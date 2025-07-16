e4fd85ce76f3f0612720f1a05e2ea3c0f98abf30
Adebayo Jamiu <adebayo.jamiu@identikosolutions.com>
2025-07-16 16:47:17 +0100

    Removed unused imports

--- app/src/main/java/com/example/delivery/HomeScreen.kt
+++ app/src/main/java/com/example/delivery/HomeScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -42,18 +41,13 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -73,10 +67,6 @@ fun HomeScreen(
     onChatClick: () -> Unit = {},
     paddingValues: PaddingValues
 ) {
-    var searchText by remember { mutableStateOf("") }
-//    var senderLocation by remember { mutableStateOf("") }
-//    var receiverLocation by remember { mutableStateOf("") }
-//    var approxWeight by remember { mutableStateOf("") }
 
     val vehicles = remember {
         listOf(
